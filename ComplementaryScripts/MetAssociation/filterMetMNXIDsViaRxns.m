1fb12bb9bb05bf4c760edeba4d8e09c9c81e2e84
Jon Robinson <jonrob@chalmers.se>
2018-05-31 09:44:20 +0200

    feat: filterMetMNXIDsViaRxns new input option "keepAtLeastOne"
    The original implementation of this function would not remove MNXIDs from metabolites if it would result in that metabolite having zero MNXIDs after filtering. The function has now been modified so that, by default, MNXIDs will be removed from a metabolite regardless if it results in zero MNXIDs associated with that met or not. This can be overridden if "keepAtLeastOne" is TRUE, where in cases such as this, the MNXID associations will not be removed in the filtered model, but will be marked in the "removed" structure, with the text "ALL SHOULD BE REMOVED!" next to the corresponding metabolite ID.

--- ComplementaryScripts/MetAssociation/filterMetMNXIDsViaRxns.m
+++ ComplementaryScripts/MetAssociation/filterMetMNXIDsViaRxns.m
@@ -1,4 +1,4 @@
-function [fmodel,removed] = filterMetMNXIDsViaRxns(model,mnx,ignoreComp)
+function [fmodel,removed] = filterMetMNXIDsViaRxns(model,mnx,ignoreComp,keepAtLeastOne)
 %filterMetMNXIDsViaRxns  Remove met MNXID associations based on their rxns.
 %
 % filterMetMNXIDsViaRxns determines which met MNXIDs (if any) should be 
@@ -7,11 +7,15 @@ function [fmodel,removed] = filterMetMNXIDsViaRxns(model,mnx,ignoreComp)
 % MNX rxn IDs, and retrieving those rxns from the MNX database. Any MNXIDs 
 % associated with the met that are not included in that set of rxns from 
 % the MNX database will be removed.
-% 
 %
+% NOTE: metabolites that occur only in reactions that have no associated
+%       rxnMNXIDs will be skipped, as there is insufficient evidence to
+%       properly filter their associated metMNXIDs.
+% 
+% 
 % USAGE:
 %
-%   [fmodel,removed] = filterMetMNXIDsViaRxns(model,mnx,ignoreComp);
+%   [fmodel,removed] = filterMetMNXIDsViaRxns(model,mnx,ignoreComp,keepAtLeastOne);
 %
 % INPUT:
 %
@@ -29,6 +33,16 @@ function [fmodel,removed] = filterMetMNXIDsViaRxns(model,mnx,ignoreComp)
 %                compartments will be lumped together, and when searching
 %                for rxns involving the metabolite, the compartment will be
 %                ignored.
+%
+%   keepAtLeastOne  (Optional, Default FALSE) In some cases, none of the
+%                   MNXIDs associated with a metabolite are found in any of
+%                   the reactions, and will result in a met with zero MNXID
+%                   associations in the filtered model. 
+%                   If keepAtLeastOne = TRUE, then in cases such as this,
+%                   the MNXID associations will not be removed in the
+%                   filtered model, but will be indicated in the "removed"
+%                   structure, with the text "ALL SHOULD BE REMOVED!" next
+%                   to the metabolite ID.
 %
 % OUTPUT:
 %
@@ -49,6 +63,9 @@ if nargin < 2 || isempty(mnx)
 end
 if nargin < 3
     ignoreComp = false;
+end
+if nargin < 4
+    keepAtLeastOne = false;
 end
 
 % initialize "removed" structure and "fmodel" outputs
@@ -132,19 +149,17 @@ else
     S = model.S;
 end
 
-% Identify all mets with two or more MNXID associations. Mets with one or
-% zero MNXIDs will be ignored.
-multi_ind = find(cellfun(@numel,model.metMNXID) > 1);
+% % Identify all mets with two or more MNXID associations. Mets with one or
+% % zero MNXIDs will be ignored.
+% multi_ind = find(cellfun(@numel,model.metMNXID) > 1);
 
 % Iterate through mets with multiple MNXIDs, and determine which MNXIDs
 % should be removed.
 h = waitbar(0,'Processing metabolites...');
-for i = 1:length(multi_ind)
-    
-    m = multi_ind(i);
+for i = 1:length(model.mets)
     
     % get indices of all rxns in which the met participates
-    rxn_ind = S(m,:) ~= 0;
+    rxn_ind = S(i,:) ~= 0;
     
     % obtain a unique list of all the rxn MNXIDs associated with those
     % rxns, as well as their indices in the MNX database structure
@@ -163,23 +178,21 @@ for i = 1:length(multi_ind)
     
     % determine which of the MNXIDs currently associated to the model met
     % are not found in any of the MNX rxns
-    rem_mets = ~ismember(model.metMNXID{m},MNX_metIDs);
+    rem_mets = ~ismember(model.metMNXID{i},MNX_metIDs);
     
     if ~any(rem_mets)
         % if no met MNXID associations should be removed, continue to next met
         continue
-    elseif all(rem_mets)
-        % This shouldn't happen, especially if "compareRxnMNXIDsWithMets" 
-        % is run first, and all the proper metMNXIDs have been added to the
-        % model. However, it is still possible, and if it does occur, we
-        % do not want to remove all MNXIDs associated to the met, so NONE
-        % will be removed. However, the information will be reported in the
-        % "removed" structure, so that these cases can be manually
-        % inspected by the user.
+    elseif all(rem_mets) && (keepAtLeastOne)
+        % In this case, all the MNXIDs associated with this met are to be
+        % removed, but the user has indicated that they do not want this to
+        % happen. Therefore,  NONE will be removed, but the information 
+        % will be reported in the "removed" structure, so these cases can
+        % be manually evaluated by the user.
         removed.mets = [removed.mets; model.mets(m)];
         removed.metMNXID = [removed.metMNXID; {'ALL SHOULD BE REMOVED!'}];
     else
-        % remove one or more (but not all) MNXID associations from the met
+        % remove one or more MNXID associations from the metabolite
         removed.mets = [removed.mets; model.mets(m)];
         removed.metMNXID = [removed.metMNXID; {model.metMNXID{m}(rem_mets)}];
         model.metMNXID{m}(rem_mets) = [];
