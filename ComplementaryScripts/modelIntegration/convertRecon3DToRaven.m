4116fee8488f46ea8af03b0a0ba8fe8a817e373b
Hao Wang <hao.wang@chalmers.se>
2018-06-20 14:13:30 +0300

    feat: add metAssoc with one-to-one association between HMR and Recon3D

--- ComplementaryScripts/modelIntegration/convertRecon3DToRaven.m
+++ ComplementaryScripts/modelIntegration/convertRecon3DToRaven.m
@@ -2,6 +2,7 @@
 % FILE NAME:    convertRecon3DToRaven.m
 % 
 % DATE CREATED: 2018-04-25
+%     MODIFIED: 2018-06-20
 % 
 % PROGRAMMER:   Hao Wang
 %               Department of Biology and Biological Engineering
@@ -9,6 +10,7 @@
 % 
 % 
 % PURPOSE: Convert Recon3D model to RAVEN format
+%          replace met with HMR info
 %
 
 % Load Recon3D
@@ -37,4 +39,28 @@ Recon3DRaven.rxnEquations=constructEquations(Recon3DRaven);
 Recon3DRaven.originalMets=Recon3D.originalMets;
 
 save('Recon3DRaven.mat','Recon3DRaven');
+
+% Create data structure of one-to-one met assocation between HMR2 and Recon3D 
+load('metAssocHMR2Recon3.mat');   %To get the manually curated met association info
+% Directly save the unique associations
+single_ind=find(cellfun(@numel,metAssocHMR2Recon3.metRecon3DID)==1);
+metAssoc.metHMRID=metAssocHMR2Recon3.metHMRID(single_ind);
+metAssoc.metRecon3DID=reformatElements(metAssocHMR2Recon3.metRecon3DID(single_ind),'cell2str');
+metAssoc.metNames=metAssocHMR2Recon3.metNames(single_ind);
+% Keep the associations between one HMR id to multiple Recon3D id
+multi_ind=find(cellfun(@numel,metAssocHMR2Recon3.metRecon3DID)>1);
+for i=1:length(multi_ind)
+		m=multi_ind(i);
+		num=numel(metAssocHMR2Recon3.metRecon3DID{m});
+		temp=cell(num,1);
+		temp(:)={metAssocHMR2Recon3.metHMRID{m}};
+		names=cell(num,1);
+		names(:)={metAssocHMR2Recon3.metNames{m}};
+		metAssoc.metHMRID=[metAssoc.metHMRID;temp];
+		metAssoc.metRecon3DID=[metAssoc.metRecon3DID;transpose(metAssocHMR2Recon3.metRecon3DID{m})];
+		metAssoc.metNames=[metAssoc.metNames;names];
+end
+save('metAssoc.mat','metAssoc');         % 2018-06-20
+% This file is designed for convinient model integration purpose,
+% and should be updated once metAssocHMR2Recon3.mat is changed!
 
