3126491e2ecc3514fd329d072429152da1a57ec1
Hao-Chalmers <hao.wang@chalmers.se>
2018-09-27 15:16:57 +0200

    feat: add the index of proton-related imbalance rxns as optional output

--- ComplementaryScripts/Functions/protonBalance4Rxns.m
+++ ComplementaryScripts/Functions/protonBalance4Rxns.m
@@ -1,21 +1,22 @@
-function [newModel, newS]=protonBalance4Rxns(model,protonMetId)
+function [newModel, newS, indRxnImbalance]=protonBalance4Rxns(model,protonMetId)
 % 
 %   protonBalance4Rxns aims to detect and rebalance the reactions that are
 %   not balanced solely due to mismatched proton(s)
 %
-%   model           a model structure
-%   protonMetId     compartment-free met id of proton
+%   model             a model structure
+%   protonMetId       compartment-free met id of proton
 % 
-%   newModel        an updated model structure
-%   newS            an updated S matrix with balanced proton for some
-%                   reactions
+%   newModel          an updated model structure
+%   newS              an updated S matrix with balanced proton for some
+%                     reactions
+%   indRxnImbalance   index of reactions with imbalanced proton(s)
 %
 %   NOTE: The COBRA function checkMassChargeBalance is used for getting
 %   imbalanced mass and charge values for each reaction. Rebalancing is 
 %   restricted to reactions whose mets are all in the same compartment.
 %   Only the S matrix is modified in the update model structure.
 %
-%   Usage: [newModel, newS]=protonBalance4Rxns(model,protonMetId)
+%   Usage: [newModel, newS, indRxnImbalance]=protonBalance4Rxns(model,protonMetId)
 %
 %   Hao Wang, 2018-09-27
 %
@@ -25,8 +26,8 @@ function [newModel, newS]=protonBalance4Rxns(model,protonMetId)
 % get imbalanced mass and charge values using checkMassChargeBalance
 [~,imbalancedMass,imbalancedCharge,~,~,~,~] = checkMassChargeBalance(model);
 
-% intermediate results
-indRxnImbalanceProton = [];
+% log the index of modified reactions
+indRxnImbalance = [];
 
 % focus on the reactions with imbalanced mass
 indImbalanceMass = getNonEmptyList(imbalancedMass);
@@ -36,13 +37,13 @@ for i=1:length(indImbalanceMass)
     m = indImbalanceMass(i);
     protonCoeff = regexprep(imbalancedMass{m},' H$','');  % imbalanced mass
     
-    % make sure that the imbalance is only contributed by proton
-    % if the mass differences equals the charge differences
-    if isequal(str2num(protonCoeff),imbalancedCharge(m))
+    % make sure that the imbalance is only contributed by proton, this is
+    % determined when the mass differences equals the charge differences
+    if isequal(str2double(protonCoeff),imbalancedCharge(m))
         
         % check if all mets in a reaction are in the same compartment
         % here only RAVEN format model structure is considered
-        checkComps=num2cell(model.metComps(find(model.S(:,m))));
+        checkComps=num2cell(model.metComps(model.S(:,m)));
         if isequal(checkComps{:})
             
             % compartment id is appended here, this only suits for HMR
@@ -58,15 +59,15 @@ for i=1:length(indImbalanceMass)
                 fullS(protonMetIndex,m) = -1*imbalancedCharge(m) + fullS(protonMetIndex,m);
             end
             
-            % record the rxn ids
-            indRxnImbalanceProton = [indRxnImbalanceProton;m];
+            % record the ids of modified rxns
+            indRxnImbalance = [indRxnImbalance;m];
         end
     end
 end
-new_S = sparse(fullS);
 
 % generating output
 newModel = model;
+newS = sparse(fullS);
 newModel.S = newS;
 
 end
