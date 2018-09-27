caecc2a5ae43a65efc0c30cd482a393ac3f8c536
Hao-Chalmers <hao.wang@chalmers.se>
2018-09-27 10:26:33 +0200

    feat: enable protonBalance4Rxns.m to balance rxns already have proton(s)

--- ComplementaryScripts/Functions/protonBalance4Rxns.m
+++ ComplementaryScripts/Functions/protonBalance4Rxns.m
@@ -1,19 +1,23 @@
-function [new_model, new_S]=protonBalance4Rxns(model,protonMetId)
+function [newModel, newS]=protonBalance4Rxns(model,protonMetId)
 % 
 %   protonBalance4Rxns aims to detect and rebalance the reactions that are
 %   not balanced solely due to mismatched proton(s)
 %
 %   model           a model structure
-%
 %   protonMetId     compartment-free met id of proton
+% 
+%   newModel        an updated model structure
+%   newS            an updated S matrix with balanced proton for some
+%                   reactions
 %
 %   NOTE: The COBRA function checkMassChargeBalance is used for getting
 %   imbalanced mass and charge values for each reaction. Rebalancing is 
-%   restricted to reactions whose mets are all in the same compartment
+%   restricted to reactions whose mets are all in the same compartment.
+%   Only the S matrix is modified in the update model structure.
 %
-%   Usage: [new_model, new_S]=protonBalance4Rxns(model,protonMetId)
+%   Usage: [newModel, newS]=protonBalance4Rxns(model,protonMetId)
 %
-%   Hao Wang, 2018-09-26
+%   Hao Wang, 2018-09-27
 %
 
 % handel input
@@ -22,7 +26,7 @@ function [new_model, new_S]=protonBalance4Rxns(model,protonMetId)
 [~,imbalancedMass,imbalancedCharge,~,~,~,~] = checkMassChargeBalance(model);
 
 % intermediate results
-rxnInd = [];
+indRxnImbalanceProton = [];
 
 % focus on the reactions with imbalanced mass
 indImbalanceMass = getNonEmptyList(imbalancedMass);
@@ -42,23 +46,28 @@ for i=1:length(indImbalanceMass)
         if isequal(checkComps{:})
             
             % compartment id is appended here, this only suits for HMR
-            % met id so far and need to be adjusted for general usage
+            % met id so far and need to be adjusted later for general usage
             proton=strcat(protonMetId,model.comps{checkComps{1}});
             
-            % make sure proton is not present in this reaction            
             protonMetIndex = find(strcmp(model.mets,proton));
+            % when proton is NOT present in this reaction   
             if fullS(protonMetIndex,m) == 0
                 fullS(protonMetIndex,m) = -1*imbalancedCharge(m);
+            % when proton is present
+            else
+                fullS(protonMetIndex,m) = -1*imbalancedCharge(m) + fullS(protonMetIndex,m);
             end
+            
+            % record the rxn ids
+            indRxnImbalanceProton = [indRxnImbalanceProton;m];
         end
     end
 end
-
 new_S = sparse(fullS);
 
 % generating output
-new_model = model;
-new_model.S = new_S;
+newModel = model;
+newModel.S = newS;
 
 end
 
