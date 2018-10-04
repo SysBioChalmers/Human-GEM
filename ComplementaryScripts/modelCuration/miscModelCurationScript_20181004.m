5bdede7a022594b71f53a19b56511c006c6aba84
Hao-Chalmers <hao.wang@chalmers.se>
2018-10-04 21:48:31 +0200

    feat: include code for updating metFrom and rxnRecon3DID fields

--- ComplementaryScripts/modelCuration/miscModelCurationScript_20181004.m
+++ ComplementaryScripts/modelCuration/miscModelCurationScript_20181004.m
@@ -14,21 +14,20 @@
 %      
 
 
-
-%% Load important items
+%% Load model and rxnAssoc
 
 % load latest version of HumanGEM
 load('humanGEM.mat');  % v0.4.0
-ihuman.rxnEqns = constructEquations(ihuman);   % construct rxn equations
+rxnEqns = constructEquations(ihuman);   % construct reaction equations
 
 % load rxnAssoc.mat
 load('ComplementaryScripts/modelIntegration/rxnAssoc.mat');
-rxnAssoc_orig = rxnAssoc;  % make backup
+
 
 
 %% Identify duplicate reaction pairs by ignoring reaction direction
 
-% get all sets of duplicated reactions
+% get the sets of duplicated reactions
 [~,~,~,dupRxnSets] = checkDuplicateRxn_mod(ihuman,'FR');
 
 % ignore sets that contain more than two duplicates
@@ -42,31 +41,29 @@ fprintf('\t* A total of %u duplicate reaction pairs were found.\n',length(dupRxn
 dupRxnInds = vertcat(dupRxnSets{:});
 dupRxnNames = ihuman.rxns(dupRxnInds);
 
-% check that duplicate sets come from different models (i.e., one from
-% HMR, and one from Recon3D), otherwise remove
+% check that duplicate sets and make sure one is from
+% HMR, and the other from Recon3D
 rem_sets = sum(startsWith(dupRxnNames,'HMR_'),2) ~= 1;
 dupRxnInds(rem_sets,:) = [];
 dupRxnNames(rem_sets,:) = [];
 fprintf('\t* %u duplicate pairs remain after removing those from the same source model (HMR or Recon3D).\n',size(dupRxnInds,1));
 
-% organize pairs so that first entry is always the HMR rxn
-% can be deleted
-flipRow = ~startsWith(dupRxnNames(:,1),'HMR_');
-dupRxnNames(flipRow,:) = fliplr(dupRxnNames(flipRow,:));
+% organize pairs so that first entry is always the HMR rxn, not needed
+% flipRow = ~startsWith(dupRxnNames(:,1),'HMR_');
+% dupRxnNames(flipRow,:) = fliplr(dupRxnNames(flipRow,:));
 
 % check if any of the associations already exist in rxnAssoc 
-% (join columns by arbitrary string "***" to enable use of ismember)
 rem_sets = ismember(join(dupRxnNames,'***'),join([rxnAssoc.rxnHMRID,rxnAssoc.rxnRecon3DID],'***'));
 dupRxnInds(rem_sets,:) = [];
 dupRxnNames(rem_sets,:) = [];
 fprintf('\t* %u duplicate pairs remain after removing those already present in rxnAssoc.mat.\n',size(dupRxnInds,1));
 
 % print out for manual curation
-for i=1:length(dupRxnInds)
-    ind1 = find(strcmp(ihuman.rxns,dupRxnNames{i,1}));
-    ind2 = find(strcmp(ihuman.rxns,dupRxnNames{i,2}));
-    fprintf('%s\t%s\n%s\t%s\n\n',dupRxnNames{i,1},ihuman.rxnEqns{ind1},dupRxnNames{i,2},ihuman.rxnEqns{ind2});
-end
+% for i=1:length(dupRxnInds)
+%    ind1 = find(strcmp(ihuman.rxns,dupRxnNames{i,1}));
+%    ind2 = find(strcmp(ihuman.rxns,dupRxnNames{i,2}));
+%    fprintf('%s\t%s\n%s\t%s\n\n',dupRxnNames{i,1},rxnEqns{ind1},dupRxnNames{i,2},rxnEqns{ind2});
+% end
 
 % get bounds for associated rxns
 dupRxnLB = ihuman.lb(dupRxnInds);
@@ -81,14 +78,39 @@ rxnAssoc.lbRecon3D = [rxnAssoc.lbRecon3D; dupRxnLB(:,2)];
 rxnAssoc.ubRecon3D = [rxnAssoc.ubRecon3D; dupRxnUB(:,2)];
 
 %% Save rxnAssoc.mat structure
-save('rxnAssoc.mat','rxnAssoc');
+save('../modelIntegration/rxnAssoc.mat','rxnAssoc');
 % print out summary
 fprintf('\t* %u duplicate reaction pairs were added to rxnAssoc.mat.\n\n',size(dupRxnInds,1));
+
+% Update the rxnRecon3DID field in the model
+for i = 1:length(dupRxnInds)
+    [~,ind] = ismember(dupRxnNames(i,1),ihuman.rxns);
+    if ~isempty(ihuman.rxnRecon3DID{ind})
+        % ensure entries are unique, and separted by semicolon if multiple
+        updatedEntry = join(unique([strsplit(ihuman.rxnRecon3DID{ind},';'),dupRxnNames(i,2)]),';');
+    else
+        updatedEntry = dupRxnNames(i,2);
+    end
+    ihuman.rxnRecon3DID(ind) = updatedEntry;
+end
+
+% update .metFrom field by filling the blank entries
+% derived from adding boundary metabolite step
+indEmpty = find(cellfun('isempty', ihuman.metFrom));
+ihuman.metFrom(indEmpty) = {'Recon3D'};
+
+
+% delete duplicated reactions
+model = removeReactionsFull(ihuman,dupRxnNames(:,2));
+fprintf('A total of %d reactions were deleted from the model.\n\n',length(dupRxnNames));
 
 
 %% Incoporate curated grRules based on the CORUM enzyme complexes database
 
-newModel = addCuratedComplexRulesToModel(ihuman, 'curated_CORUM_grRules_20180924');
+% add curated grRules from automatic incoporation of enzyme
+% complexes info in CORUM database and manual checking, as
+% well as additional redundant gene removal step
+newModel = addCuratedComplexRulesToModel(model, 'curated_CORUM_grRules_20180924');
 
 % Get the index of reactions with modified grRules
 indModifiedRxn = find(~strcmp(ihuman.grRules, newModel.grRules));
@@ -98,7 +120,7 @@ fprintf('A total of %d grRules are changed with curation from CORUM database.\n'
 
 %% Save model
 ihuman = newModel;
-save('ihuman.mat','ihuman');
+save('../../ModelFiles/mat/ihuman.mat','ihuman');
 
 
 
