a8a725fac4bb708c084bd7d19dc89e1f3d092d2a
Hao-Chalmers <hao.wang@chalmers.se>
2018-10-19 11:58:04 +0200

    fix: correct the path for saving the model

--- ComplementaryScripts/GPRs/EnzymeComplexes/updateGrRulesAndGenes_20181018.m
+++ ComplementaryScripts/GPRs/EnzymeComplexes/updateGrRulesAndGenes_20181018.m
@@ -61,7 +61,7 @@ ihuman.grRules(rxn_ind) = regexprep(ihuman.grRules(rxn_ind),' and ENSG0000019876
 
 %% Export model
 
-save('ModelFiles/mat/humanGEM.mat','ihuman');
+save('../../../ModelFiles/mat/humanGEM.mat','ihuman');
 
 
 
