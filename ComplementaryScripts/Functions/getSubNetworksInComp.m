7043efeb82a5086db35e7a11341adae4d57992ba
Hao-Chalmers <hao.wang@chalmers.se>
2018-11-12 15:48:41 +0100

    refactor: use subfunction to print out reactions in each sub-network

--- ComplementaryScripts/Functions/getSubNetworksInComp.m
+++ ComplementaryScripts/Functions/getSubNetworksInComp.m
@@ -1,20 +1,28 @@
 function [subNetworks]=getSubNetworksInComp(model,comp,metsToRemove,includePartial)
 % getSubNetworksInComp
 %	Check network connectivity in specified compartment and output
-%   the sub-graphs in JSON fommat for map generation purpose
+%   the sub-networks in JSON fommat for map generation purpose
 %
-%   Input:
+% Input:
 %   model           a model structure
 %   comp            string with the compartment id
 %   metsToRemove    cell array of mets to be excluded from the network
 %   includePartial  if true, include reactions with metabolites partially
 %                   present in the specified compartment (opt, default false)
 %
-%	NOTE: The output filename is "SubNetworks.json" in defined JSON format 
+% Output:
+%   subNetworks     a structure for the sub-networks
+%      id           the id for identifying the sub-networks
+%      number       number of reactions in each sub-network
+%      rxns         cell array of reaction identifiers in each sub-network
+%      subSystems   cell array of subSystems for reactions in each sub-network
 %
-%	Usage: [subNetworks]=getSubNetworksInComp(model,comp,metsToRemove,includePartial)
 %
-%	Hao Wang, 2018-11-12
+% NOTE: The output filename "SubNetworks.json" and in defined JSON format 
+%
+% Usage: [subNetworks]=getSubNetworksInComp(model,comp,metsToRemove,includePartial)
+%
+% Hao Wang, 2018-11-12
 %
 
 % handle input arguments
@@ -31,7 +39,8 @@ end
 % get the subnetworks in specified compartment
 compNetwork = getCompNetwork(model,comp,includePartial);
 
-% refine the network by excluding metabolites metsToRemove
+% refine the network by excluding any metabolites to be removed and
+% identifiy any ractions that have only currency metabolites
 excludeMets = intersect(metsToRemove, compNetwork.mets);
 if isempty(excludeMets)
     frintf('The provided metabolites are not found in the model\n');
@@ -39,54 +48,60 @@ if isempty(excludeMets)
     currencyRxns = '';
 else
     reducedNetwork = removeMets(compNetwork,excludeMets,0,1);
-
-    % identifiy ractions that have only currency metabolites
     currencyRxns = setdiff(compNetwork.rxns, reducedNetwork.rxns);
     [~, currencyRxnInd] = ismember(currencyRxns, model.rxns);
 end
 
 % get the sub-graphs
 subGraphs = getAllSubGraphs(reducedNetwork);
-num = size(subGraphs,2);
-subNetworks.id = transpose(1:num);
 
-% generate output in JSON format
+% generate output
+graphNum = size(subGraphs,2);
+subNetworks.id = transpose(1:graphNum);
+
+% write in JSON format
 fid = fopen('Subnetworks.json','w');
 fprintf(fid, '{\n');
 
-for i=1:num
-    fprintf(fid,['\t"' num2str(i) '":[\n']);
+for i=1:graphNum
     index = find(subGraphs(:,i));
     [~, rxnList]=find(reducedNetwork.S(index,:));
-    subNetworks.number(i,1) = length(unique(rxnList));
-    subNetworks.rxnList{i,1} = unique(rxnList);
-    for j=1:numel(subNetworks.rxnList{i,1})
-        ind = subNetworks.rxnList{i,1}(j);
-        if j==numel(subNetworks.rxnList{i,1})
-            fprintf(fid,['\t\t["' reducedNetwork.rxns{ind} '", "' reducedNetwork.subSystems{ind} '"]\n']);
-        else
-            fprintf(fid,['\t\t["' reducedNetwork.rxns{ind} '", "' reducedNetwork.subSystems{ind} '"],\n']);
-        end
-    end
-    if i==num && isempty(currencyRxns)
+    I = unique(rxnList);
+    subNetworks.number(i,1) = length(I);
+    subNetworks.rxns{i,1} = reducedNetwork.rxns(I);
+    subNetworks.subSystems{i,1} = reducedNetwork.subSystems(I);
+    
+    fprintf(fid,['\t"' num2str(i) '":[\n']);
+    writeSubNetworks(fid, reducedNetwork.rxns(I), reducedNetwork.subSystems(I));
+    if i==graphNum && isempty(currencyRxns)
         fprintf(fid, '\t]\n');
     else
         fprintf(fid, '\t],\n');
     end
 end
 
-fprintf(fid,['\t"currencyRxns":[\n']);
-for k=1:numel(currencyRxns)
-    if k==numel(currencyRxns)
-        fprintf(fid,['\t\t["' currencyRxns{k} '", "' model.subSystems{currencyRxnInd(k)} '"]\n']);
-    else
-        fprintf(fid,['\t\t["' currencyRxns{k} '", "' model.subSystems{currencyRxnInd(k)} '"],\n']);
-    end
+if ~isempty(currencyRxns)
+    fprintf(fid,['\t"currencyRxns":[\n']);
+    writeSubNetworks(fid, currencyRxns, model.subSystems(currencyRxnInd));
+    fprintf(fid, '\t]\n');
 end
-fprintf(fid, '\t]\n');
-
 fprintf(fid, '}\n');
-
 fclose(fid);
 
+end
 
+
+function writeSubNetworks(file, rxnList, subSystemList)
+if ~isequal(numel(rxnList), numel(subSystemList))
+    error('Input reaction and subSystems lists do not match!');
+else
+    for i=1:numel(rxnList)
+        if i==numel(rxnList)
+            fprintf(file,['\t\t["' rxnList{i} '", "' subSystemList{i} '"]\n']);
+        else
+            fprintf(file,['\t\t["' rxnList{i} '", "' subSystemList{i} '"],\n']);
+        end
+    end
+end
+
+end
