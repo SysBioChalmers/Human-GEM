fa56ff4d1be57b40a9169219652638e9cb70d844
Jon Robinson <jonrob@chalmers.se>
2019-05-17 17:20:55 +0200

    feat: docRxnChanges and writeRxnChanges now document grRule changes
    The docRxnChanges function can also now handle newly added reactions.

--- ComplementaryScripts/Functions/docRxnChanges.m
+++ ComplementaryScripts/Functions/docRxnChanges.m
@@ -1,10 +1,9 @@
 function rxnChanges = docRxnChanges(model1,model2,rxnNotes)
 %docRxnChanges  Document changes to model reactions.
 %
-%   docRxnChanges looks for differences between two models, which should be
-%   immediately before and after reaction(s) have been modified. It will
-%   then report the original and new reaction equations and bounds, as well
-%   as any user-supplied notes for each reaction.
+%   docRxnChanges identifies reaction-related differences between two
+%   models. The function reports the original and new reaction equations, 
+%   bounds, and grRules, and any user-supplied notes for each reaction.
 %
 % USAGE:
 %
@@ -12,7 +11,7 @@ function rxnChanges = docRxnChanges(model1,model2,rxnNotes)
 %
 % INPUT:
 %
-%   model1     Model structure prior to changing reaction(s).
+%   model1     Model structure before changing reaction(s).
 %
 %   model2     Model structure after changing reaction(s).
 %
@@ -33,16 +32,19 @@ function rxnChanges = docRxnChanges(model1,model2,rxnNotes)
 %
 %   rxnChanges  A cell structure containing the original and new reaction
 %               properties, with the following fields:
-%                 rxn       reaction ID
-%                 eqnOrig   original reaction equation
-%                 eqnNew    new reaction equation
-%                 lbOrig    original lower bound
-%                 lbNew     new lower bound
-%                 ubOrig    original upper bound
-%                 ubNew     new upper bound
-%                 notes     notes associated with the change
+%                 rxn         reaction ID
+%                 eqnOrig     original reaction equation
+%                 eqnNew      new reaction equation
+%                 lbOrig      original lower bound
+%                 lbNew       new lower bound
+%                 ubOrig      original upper bound
+%                 ubNew       new upper bound
+%                 grRuleOrig  original grRules
+%                 grRuleNew   new grRules
+%                 notes       optional notes associated with the change
 %
-% Jonathan Robinson, 2018-10-15
+%
+% Jonathan Robinson, 2019-05-17
 
 
 % handle input arguments
@@ -52,7 +54,6 @@ end
 
 
 % get rxns from rxnNotes, and include them in the list of changed rxns
-model1.rxnNotes = repmat({''},length(model1.rxns),1);  % intialize field
 if ~isempty(rxnNotes)
     % check if any rxn IDs are repeated in the rxnNotes array, and if so,
     % merge those notes using a semicolon separator (;)
@@ -67,8 +68,6 @@ if ~isempty(rxnNotes)
         rxnNotes = rxnNotes(uniq_ind,:);  % remove duplicated entries after merging notes
     end
     chg_rxn = rxnNotes(:,1);
-    [~,ind] = ismember(chg_rxn,model1.rxns);
-    model1.rxnNotes(ind) = rxnNotes(:,2);
 else
     chg_rxn = [];
 end
@@ -82,16 +81,21 @@ nondel_rxn = model1.rxns(is_present);  % get list of non-deleted reactions
 model1.eqns = constructEquations(model1);
 model2.eqns = constructEquations(model2);
 
-% check which reactions have a changed equation
+% check for added reactions
+add_rxn = setdiff(model2.rxns,model1.rxns);
+
+% check which reactions have a changed equation or grRule
 ind = (1:length(model1.rxns));
 chg_eqn = ~arrayfun(@(i) isequal(model1.eqns(i),model2.eqns(rxn_ind(i))),ind(is_present));
+chg_rule = ~arrayfun(@(i) isequal(model1.grRules(i),model2.grRules(rxn_ind(i))),ind(is_present));
 
 % check which reactions have a changed bound
 chg_bnd = model1.lb(is_present) ~= model2.lb(rxn_ind(is_present)) | ...
           model1.ub(is_present) ~= model2.ub(rxn_ind(is_present));
 
 % collect unique list of all changed reactions
-chg_rxn = unique([chg_rxn; nondel_rxn(chg_eqn); nondel_rxn(chg_bnd); del_rxn]);
+model1.rxns = [model1.rxns; add_rxn];
+chg_rxn = unique([chg_rxn; nondel_rxn(chg_eqn); nondel_rxn(chg_bnd); nondel_rxn(chg_rule); del_rxn; add_rxn]);
 [~,chg_rxn_ind_orig] = ismember(chg_rxn,model1.rxns);
 [~,chg_rxn_ind_new] = ismember(chg_rxn,model2.rxns);
 
@@ -100,17 +104,35 @@ chg_rxn_ind_new(chg_rxn_ind_new == 0) = length(model2.rxns)+1;
 model2.eqns(end+1) = {'(DELETED)'};
 model2.lb(end+1) = 0;
 model2.ub(end+1) = 0;
+model2.grRules(end+1) = {''};
+
+% add dummy entries to model1 to handle added reactions
+model1.eqns(end+1:end+numel(add_rxn)) = {'(ADDED)'};
+model1.lb(end+1:end+numel(add_rxn)) = 0;
+model1.ub(end+1:end+numel(add_rxn)) = 0;
+model1.grRules(end+1:end+numel(add_rxn)) = {''};
 
 
 % organize information into output structure
 rxnChanges = {};
-rxnChanges.rxns    = model1.rxns(chg_rxn_ind_orig);
-rxnChanges.eqnOrig = model1.eqns(chg_rxn_ind_orig);
-rxnChanges.eqnNew  = model2.eqns(chg_rxn_ind_new);
-rxnChanges.lbOrig  = model1.lb(chg_rxn_ind_orig);
-rxnChanges.lbNew   = model2.lb(chg_rxn_ind_new);
-rxnChanges.ubOrig  = model1.ub(chg_rxn_ind_orig);
-rxnChanges.ubNew   = model2.ub(chg_rxn_ind_new);
-rxnChanges.notes   = model1.rxnNotes(chg_rxn_ind_orig);
+rxnChanges.rxns       = model1.rxns(chg_rxn_ind_orig);
+rxnChanges.eqnOrig    = model1.eqns(chg_rxn_ind_orig);
+rxnChanges.eqnNew     = model2.eqns(chg_rxn_ind_new);
+rxnChanges.lbOrig     = model1.lb(chg_rxn_ind_orig);
+rxnChanges.lbNew      = model2.lb(chg_rxn_ind_new);
+rxnChanges.ubOrig     = model1.ub(chg_rxn_ind_orig);
+rxnChanges.ubNew      = model2.ub(chg_rxn_ind_new);
+rxnChanges.grRuleOrig = model1.grRules(chg_rxn_ind_orig);
+rxnChanges.grRuleNew  = model2.grRules(chg_rxn_ind_new);
 
+
+% add reaction notes
+rxnChanges.notes = repmat({''},size(rxnChanges.rxns));
+if ~isempty(rxnNotes)
+    [~,rxn_ind] = ismember(rxnNotes(:,1),rxnChanges.rxns);
+    if any(rxn_ind == 0)
+        error('One or more reaction IDs in rxnNotes were not found in either model.');
+    end
+    rxnChanges.notes(rxn_ind) = rxnNotes(:,2);
+end
 
