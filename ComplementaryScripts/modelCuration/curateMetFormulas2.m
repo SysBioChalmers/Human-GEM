dc097d8e0eb806ecb66ef85fd17febe34af21eaf
Jon Robinson <jonrob@chalmers.se>
2019-07-22 17:17:19 +0200

    feat: curateMetFormulas2 script to improve metabolite formula accuracy

---
+++ ComplementaryScripts/modelCuration/curateMetFormulas2.m
@@ -0,0 +1,23 @@
+%
+% FILE NAME:    curateMetFormulas2.m
+% 
+% DATE CREATED: 2019-07-22
+%     MODIFIED: 2019-07-22
+% 
+% PROGRAMMER:   Jonathan Robinson
+%               Department of Biology and Biological Engineering
+%               Chalmers University of Technology
+% 
+% PURPOSE: This script curates the formulas of several metabolites to
+%          improve model accuracy and reaction mass balances.
+%
+
+
+
+
+
+
+
+
+
+
