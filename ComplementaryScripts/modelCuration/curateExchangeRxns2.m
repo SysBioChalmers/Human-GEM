6dcbe463bd741704f30d5ff30362aee565ac189c
Jon Robinson <jonrob@chalmers.se>
2019-08-01 14:11:57 +0200

    fix: curateExchangeRxns2 update script to edit annotation JSON files

--- ComplementaryScripts/modelCuration/curateExchangeRxns2.m
+++ ComplementaryScripts/modelCuration/curateExchangeRxns2.m
@@ -40,7 +40,18 @@
 load('humanGEM.mat');
 ihuman_orig = ihuman;  % to track changes
 
-% initialize reaction change notes
+% load reaction and metabolite annotation structures
+metAssoc = jsondecode(fileread('../../ComplementaryData/annotation/humanGEMMetAssoc.JSON'));
+rxnAssoc = jsondecode(fileread('../../ComplementaryData/annotation/humanGEMRxnAssoc.JSON'));
+metAssocFields = fieldnames(metAssoc);
+rxnAssocFields = fieldnames(rxnAssoc);
+
+% verify that model and annotation structures are aligned
+if ~isequal(metAssoc.mets, ihuman.mets) || ~isequal(rxnAssoc.rxns, ihuman.rxns)
+    error('HumanGEM mets and rxns are not aligned with those in the annotation structures!');
+end
+
+% initialize model change notes
 changeNotes = {};
 
 
@@ -62,6 +73,9 @@ mets_orig = {'temp002x';'temp003x';'temp004x';'temp005x'};
 mets_new  = {'m10000x' ;'m10001x' ;'m10002x' ; 'm10003x'};
 mets_ind = getIndexes(ihuman,mets_orig,'mets');
 ihuman.mets(mets_ind) = mets_new;
+metAssoc.mets(mets_ind) = mets_new;
+
+% append change notes
 changeNotes = [changeNotes; [[mets_orig;mets_new], repmat({'Updated "temp" metabolite IDs.'},numel([mets_orig;mets_new]),1)]];
 
 
@@ -78,7 +92,18 @@ metsToAdd.mets     = {'m10000s';'m10001s' ;'m10002s'    ;'m10003s'              
 metsToAdd.metNames = {'others' ;'steroids';'xenobiotics';'arachidonate derivatives'};
 metsToAdd.compartments = 's';
 
+% add new metabolites to model
 ihuman = addMets(ihuman,metsToAdd);
+
+% add new metabolites to metAssoc structure
+newMetInd = numel(metAssoc.mets) + (1:numel(metsToAdd.mets));
+for i = 1:numel(metAssocFields)
+    metAssoc.(metAssocFields{i})(newMetInd) = {''};
+end
+metAssoc.mets(newMetInd) = metsToAdd.mets;
+metAssoc.metsNoComp(newMetInd) = regexprep(metsToAdd.mets, '_*\w$', '');
+
+% append change notes
 changeNotes = [changeNotes; [metsToAdd.mets, repmat({'Extracellular version of metabolite added to enable the addition of an exchange reaction.'},numel(metsToAdd.mets),1)]];
 
 
@@ -94,7 +119,18 @@ metsToAdd.mets     = {'m00591x'                ;'m01435x'  ;'m02328x'    ;'chylo
 metsToAdd.metNames = {'20-hydroxy-arachidonate';'chenodiol';'LacCer pool';'Chylomicron Lipoprotein'};
 metsToAdd.compartments = 'x';
 
+% add new metabolites to model
 ihuman = addMets(ihuman,metsToAdd);
+
+% add new metabolites to metAssoc structure
+newMetInd = numel(metAssoc.mets) + (1:numel(metsToAdd.mets));
+for i = 1:numel(metAssocFields)
+    metAssoc.(metAssocFields{i})(newMetInd) = {''};
+end
+metAssoc.mets(newMetInd) = metsToAdd.mets;
+metAssoc.metsNoComp(newMetInd) = regexprep(metsToAdd.mets, '_*\w$', '');
+
+% append change notes
 changeNotes = [changeNotes; [metsToAdd.mets, repmat({'Boundary compartment version of metabolite added to enable the addition of an exchange reaction.'},numel(metsToAdd.mets),1)]];
 
 
@@ -121,15 +157,20 @@ rxnsToAdd.ub = rxnData{4};
 rxnsToAdd.subSystems = rxnData{5};
 rxnsToAdd.equations = rxnData{6};
 
-% add reactions
+% add new reactions to model
 ihuman = addRxns(ihuman,rxnsToAdd,3);
-
-% update other non-standard fields
-ihuman.rxnRecon3DID(end+1:end+nRxns) = {''};
 ihuman.prRules(end+1:end+nRxns) = {''};
 ihuman.rxnProtMat(end+1:end+nRxns,:) = 0;
 ihuman.priorCombiningGrRules(end+1:end+nRxns) = {''};
 
+% add new reactions to rxnAssoc structure
+newRxnInd = numel(rxnAssoc.rxns) + (1:numel(rxnsToAdd.rxns));
+for i = 1:numel(rxnAssocFields)
+    rxnAssoc.(rxnAssocFields{i})(newRxnInd) = {''};
+end
+rxnAssoc.rxns(newRxnInd) = rxnsToAdd.rxns;
+
+% append change notes
 changeNotes = [changeNotes; [rxnData{1}, repmat({'New exchange reaction to facilitate transport of metabolite between boundadry and extracellular compartment.'},nRxns,1)]];
 
 
@@ -152,7 +193,15 @@ changeNotes = [changeNotes; [rxnData{1}, repmat({'New exchange reaction to facil
 %   XOLEST2HSTDle:     cholesterol-ester pool[c] => cholesterol-ester pool[s]
 %
 % Therefore, the reaction will be removed from the model.
-ihuman = removeReactionsFull(ihuman,'HMR_9736');
+rxnInd = getIndexes(ihuman,'HMR_9736','rxns');
+ihuman = removeReactionsFull(ihuman,rxnInd);
+
+% remove reaction from rxnAssoc structure
+for i = 1:numel(rxnAssocFields)
+    rxnAssoc.(rxnAssocFields{i})(rxnInd) = [];
+end
+
+% append change notes
 changeNotes = [changeNotes; [{'HMR_9736'},{'Reaction is redundant and involves transport between lysosome and boundary, and was therefore DELETED.'}]];
 
 
@@ -186,6 +235,26 @@ changeNotes = [changeNotes; [rxns, repmat({'updated reaction to generate pool me
 
 modelChanges = docModelChanges(ihuman_orig,ihuman,changeNotes);
 writeModelChanges(modelChanges,'../../ComplementaryData/modelCuration/curateExchangeReactions2_rxnChanges.tsv');
+
+
+%% Export reaction and metabolite annotation JSON files
+
+% first verify that annotation structures are still aligned with the model
+if ~isequal(metAssoc.mets, ihuman.mets) || ~isequal(rxnAssoc.rxns, ihuman.rxns)
+    error('HumanGEM mets and rxns are not aligned with those in the annotation structures!');
+end
+
+% write metAssoc to JSON
+jsonStr = jsonencode(metAssoc);
+fid = fopen('../../ComplementaryData/annotation/humanGEMMetAssoc.JSON', 'w');
+fwrite(fid, prettyJson(jsonStr));
+fclose(fid);
+
+% write rxnAssoc to JSON
+jsonStr = jsonencode(rxnAssoc);
+fid = fopen('../../ComplementaryData/annotation/humanGEMRxnAssoc.JSON', 'w');
+fwrite(fid, prettyJson(jsonStr));
+fclose(fid);
 
 
 %% Export updated HumanGEM
