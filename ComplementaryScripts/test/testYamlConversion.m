7e554ba06d49ef3cdff96304c2b16436a0405226
Hao-Chalmers <hao.wang@chalmers.se>
2019-12-18 09:27:38 +0100

    fix: remove the section for resolving the temporary fields
    - So far, the conflicting fields for Mat-Yaml conversion have been resolved (deleted or modified)

--- ComplementaryScripts/test/testYamlConversion.m
+++ ComplementaryScripts/test/testYamlConversion.m
@@ -17,16 +17,6 @@ importedHumanGEM = importHumanYaml('testYamlConversion.yml');
 % remove intermediate Yaml file
 delete testYamlConversion.yml
 
-%===this section will be removed after resolving the temporary fields
-% extract shared fields
-ihumanFields = fieldnames(ihuman);
-importedModelFields = fieldnames(importedHumanGEM);
-sharedFields = intersect(ihumanFields, importedModelFields);
-
-% trim off unique fields
-HumanGEM = rmfield(ihuman, setdiff(ihumanFields, sharedFields));
-importedHumanGEM = rmfield(importedHumanGEM, setdiff(importedModelFields, sharedFields)); 
-%========
 
 % compare the imported model from yaml with the original one
 if isequal(HumanGEM, importedHumanGEM)
