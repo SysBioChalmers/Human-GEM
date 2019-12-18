204a4f45c600a7d32635dd2b1a3b214114ee3ff0
Hao-Chalmers <hao.wang@chalmers.se>
2019-12-18 09:24:55 +0100

    feat: add code for checking the content of rxnMiriams and metMiriams

--- ComplementaryScripts/modelCuration/enableYamlBasedWorkflow.m
+++ ComplementaryScripts/modelCuration/enableYamlBasedWorkflow.m
@@ -22,10 +22,47 @@ load('HumanGEM.mat');
 
 %% The following fields are to be deprecated
 
-% the information in fields "compOutside", "metMiriams", "rxnMiriams" is 
-% either duplicated or unnecessary, so they will be deleted
+% the "compOutside" field stores static information that can be deduced
+% from common sense, no need to keep in the model
 
-% remove the three fields
+
+% check "rxnMiriams" field
+% get index of non-empty elements
+index = find(~cellfun(@isempty, ihuman.rxnMiriams));
+rxnMiriams = ihuman.rxnMiriams(index);
+
+% check the rxnMiriams types
+allNames_rxnMiriams  = (cellfun(@(x)x.name, rxnMiriams,'UniformOutput', 0));
+rxnMiriamsNames = vertcat(allNames_rxnMiriams{:});
+unique(rxnMiriamsNames)
+% only one type "pmid" was found in "rxnMiriams" field, its informaiton is
+% duplicated to the "rxnReferences" field
+
+
+% check "metMiriams" field
+% get index of non-empty elements
+ind = find(~cellfun(@isempty, ihuman.metMiriams));
+metMiriams = ihuman.metMiriams(ind);
+
+% check the metMiriams types
+allNames_metMiriams  = (cellfun(@(x)x.name, metMiriams,'UniformOutput', 0));
+metMiriamsNames = vertcat(allNames_metMiriams{:});
+unique(metMiriamsNames)
+% hmdb
+% kegg.compound
+% kegg.glycan
+% lipidmaps
+% obo.chebi:CHEBI
+% obo.chebi:CHEBi
+% obo.chebi:ChEBI
+% there are four types (HMDB, KEGG, LipidMaps, ChEBI) of identifiers are
+% found in "metMiriams" field, this information has been continuesly
+% curated and stored in annotation files: "humanGEMRxnAssoc.JSON" and
+% "humanGEMMetAssoc.JSON"
+
+
+% since the information in fields "compOutside", "metMiriams", "rxnMiriams"
+% is either duplicated or unnecessary, so they will be deleted
 ihuman = rmfield(ihuman, {'compOutside','rxnMiriams','metMiriams'});
 
 
@@ -37,13 +74,13 @@ ihuman = rmfield(ihuman, {'compOutside','rxnMiriams','metMiriams'});
 % get the index of elements that contain quotation marks
 indRef = find(contains(ihuman.rxnReferences, '"'));
 fprintf('A total of %d elements are found with quotation marks.\n', length(indRef));
+ihuman.rxnReferences(indRef)
 ihuman.rxnReferences(indRef) = regexprep(ihuman.rxnReferences(indRef),'"','');
-%ihuman.rxnReferences(indRef)
+
 
 % Some elements in "rxnConfidenceScores" field have values in 'NaN', which
 % may lead to inconsistency during mat-yml format converstion. Here these
 % elements are set to zero
-
 indNaN = isnan(ihuman.rxnConfidenceScores);
 ihuman.rxnConfidenceScores(indNaN) = 0;
 
