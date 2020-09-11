36b0ddd99833cc965229cc9c66d03181cd97236a
Jonathan Robinson <jonrob@chalmers.se>
2020-09-11 15:45:12 +0200

    fix: getModelFromOrthology prevent error if templateModel missing fields

--- code/getModelFromOrthology.m
+++ code/getModelFromOrthology.m
@@ -35,7 +35,7 @@ end
 % pre-process the template model
 
 % remove non-standard fields, if any
-fieldsToRemove = {'rxnFrom','metFrom'};
+fieldsToRemove = setdiff({'rxnFrom','metFrom'}, fieldnames(templateModel));
 templateModel = rmfield(templateModel, fieldsToRemove);
 
 % clean metadata fields
