8656cb4dd170850b1b1581d532372926d5a730ad
Hao-Chalmers <hao.wang@chalmers.se>
2021-06-16 15:00:43 +0200

    feat: add code for replacing with MA ids

---
+++ .deprecated/code/modelCuration/implementeMAID.m
@@ -0,0 +1,59 @@
+%
+% FILE NAME:    implementeMAID.m
+% 
+% PURPOSE: This script is to implement MA reaction and metabolite ids to
+%          Human-GEM, as planned in 265#.
+%
+
+
+%% Load model and tsv annotaiton files
+
+% load HumanGEM
+ihuman = importYaml('Human-GEM.yml');
+ihuman_orig = ihuman;  % to track changes
+
+% load tsv files
+rxnAssoc = importTsvFile('reactions.tsv');
+metAssoc = importTsvFile('metabolites.tsv');
+
+
+%% swap id columns
+
+% sanity check and remove dash from MA ids
+if isequal(rxnAssoc.rxns, ihuman.rxns) && isequal(metAssoc.mets, ihuman.mets)
+    fprintf('sanity check passed and move on.\n');
+    metMAID = replace(metAssoc.metMAID, 'MA-M', 'MAM');
+    rxnMAID = replace(rxnAssoc.rxnMAID, 'MA-R', 'MAR');
+end
+
+% back up retired ids
+rxnAssoc.rxnRetired = rxnAssoc.rxns;
+metAssoc.metRetired = metAssoc.mets;
+
+% implement new ids
+ihuman.rxns   = rxnMAID;
+rxnAssoc.rxns = rxnMAID;
+
+ihuman.mets   = metMAID;
+metAssoc.mets = metMAID;
+
+
+%% check if everything is ok
+% sanity check and remove dash from MA ids
+if isequal(rxnAssoc.rxns, ihuman.rxns) && isequal(metAssoc.mets, ihuman.mets)
+    fprintf('sanity check passed.\n');
+end
+
+compareArrayStructure(ihuman, ihuman_orig);
+% everything looks good
+
+
+%% save model and annotaiton files
+exportYaml(ihuman,'../../../model/Human-GEM.yml');
+
+rxnAssoc = rmfield(rxnAssoc, {'rxnMAID'});
+exportTsvFile(rxnAssoc, '../../../model/reactions.tsv');
+
+metAssoc = rmfield(metAssoc, {'metMAID'});
+exportTsvFile(metAssoc, '../../../model/metabolites.tsv');
+
