0aff09d32472264b1368d68322188dea31265b84
Hao-Chalmers <hao.wang@chalmers.se>
2021-06-30 16:11:32 +0200

    refactor: revert the inclusion of TBD gene and keep using blank rules
    - This is because the TBD pseudogene may confuse users or functions that handle the `grRules`

--- code/gapfill4EssentialTasks.m
+++ code/gapfill4EssentialTasks.m
@@ -82,18 +82,12 @@ if ismember('id',fieldnames(outModel))
 end
 
 
-%% replace grRules of newly introduced reactions with "TBD"
+%% empty newly introduced grRules
 
 % clean the grRules associated with gap-filling reactions
 gapfilledRxns = setdiff(outModel.rxns, model.rxns);
 [~, ind] = ismember(gapfilledRxns, outModel.rxns);
 outModel.grRules(ind) = {''};
-
-% Assign "TBD" gene to the grRules which previously were non-empty in the
-% reference model. This gene name is abbreviated from "to be determined"
-[~, ind2] = ismember(gapfilledRxns, refModel.rxns);
-nonEmptyInd = getNonEmptyList(refModel.grRules(ind2));
-outModel.grRules(ind(nonEmptyInd)) = {'TBD'};
 
 % re-generate gene and rxnGeneMat fields
 [outModel.genes, outModel.rxnGeneMat] = getGenesFromGrRules(outModel.grRules);
