30c401f732cb1d59936679830bac6c61a256580f
Johan Gustafsson <gustajo@chalmers.se>
2022-06-06 11:09:23 +0200

    style: Removed some commented out code

--- code/tINIT/removeDrugReactions.m
+++ code/tINIT/removeDrugReactions.m
@@ -10,13 +10,6 @@ drugNames = {'pravastatin';'Gliclazide';'atorvastatin';'fluvastatin';'fluvastain
              'acetaminophen';'cerivastatin';'Tacrolimus';'ibuprofen';'lovastatin';'Losartan';'nifedipine';...
              'pitavastatin';'rosuvastatin';'Torasemide';'Midazolam'};
 drugMets = contains(inModel.metNames,drugNames,'IgnoreCase',true);
-%sum(drugMets)%456
-%inModel.metNames(drugMets)
 drugRxns = (sum(inModel.S(drugMets,:) ~= 0,1) > 0).';
-%inModel.rxns(drugRxns)
-%constructEquations(inModel,inModel.rxns(drugRxns))
-%sum(drugRxns)%705
-%inModel.grRules(drugRxns) %many of these have GPRs (although most of them don't)
-
 outModel = removeReactions(inModel,inModel.rxns(drugRxns));
 end
