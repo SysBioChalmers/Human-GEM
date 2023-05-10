22cb6998768696d99cd799b140a95da2555aa362
Hao Wang <haowang.bioinfo@gmail.com>
2023-05-10 13:33:55 +0200

    feat: add function for estimating essential genes

---
+++ code/estimateEssentialGenes.m
@@ -0,0 +1,90 @@
+function [eGenes, INIT_output] = estimateEssentialGenes(model, dataFile, taskStruct)
+% generate tINIT models and estimate essential genes
+%
+% Input:
+%
+%   modelfiles   Human-GEM model to be analyzed.
+%
+%   dataFile     (opt, default Hart2015_RNAseq.txt)
+%
+%   taskStruct   metabolic task structure (opt, default is Essential tasks)
+%
+% Output:
+%
+%   eGenes          results structure with the following fields:
+%       taskList    list of metabolic tasks that were tested
+%       tissues     list of tissues (model IDs) corresponding to each model
+%       geneList    cell array of the list of genes from each model
+%       essentialGenes   cell array with one entry per model, where each
+%                        entry is a logical matrix with rows corresponding
+%                        to genes (in geneList) and columns to tasks (in
+%                        taskList). Entries in the matrix are true when a
+%                        gene is essential for a task, and false otherwise.
+%
+%   INIT_output     structure containing tINIT models (or information
+%                   necessary to regenerate tINIT models) for which gene
+%                   essentiality will be evaluated.
+%
+% Note: This function may take long computation time.
+%
+
+
+if nargin < 2
+    % load Hart et al. RNA-Seq cell line data
+    dataFile = 'Hart2015_RNAseq.txt';
+end
+
+if nargin < 3
+    taskStruct = parseTaskList('metabolicTasks_Essential.txt');
+end
+
+
+% pre-process RNA-Seq data
+tmp = readtable(dataFile);
+arrayData.genes = tmp.gene;
+arrayData.tissues = tmp.Properties.VariableNames(2:end)';
+arrayData.levels = table2array(tmp(:,2:end));
+
+
+% Run some preliminary steps
+[~,deletedDeadEndRxns] = simplifyModel(model,true,false,true,true,true);
+cModel = removeReactions(model,deletedDeadEndRxns,false,true);
+[taskReport, essentialRxnMat] = checkTasks(cModel,[],true,false,true,taskStruct);
+
+
+% add pre-processing results to arrayData structure
+arrayData.deletedDeadEndRxns = deletedDeadEndRxns;
+arrayData.taskReport = taskReport;
+arrayData.essentialRxnMat = essentialRxnMat;
+arrayData.threshold = 1;
+        
+
+% run tINIT 
+model = addBoundaryMets(model);
+params = {};
+INIT_output = {};
+    
+for i = 1:length(arrayData.tissues)
+        
+    % First try to run tINIT with shorter time limit. If it fails, then
+    % try again with a longer time limit.
+    try
+        params.TimeLimit = 1000;
+        init_model = getINITModel2(model,arrayData.tissues{i},[],[],arrayData,[],true,[],true,true,taskStruct,params);
+    catch
+        params.TimeLimit = 5000;
+        init_model = getINITModel2(model,arrayData.tissues{i},[],[],arrayData,[],true,[],true,true,taskStruct,params);
+    end
+        
+    init_model.id = arrayData.tissues{i};
+    INIT_output.id{i,1} = init_model.id;
+    INIT_output.model{i,1} = init_model;
+            
+end
+    
+
+% get essential genes for each model and task
+eGenes = getTaskEssentialGenes(INIT_output, refModel, taskStruct);
+eGenes.refModel = model;
+    
+end
