82e12e8471d97ca83beff429fd5b3e9f4384eeb1
Eduard Kerkhoven <eduardk@chalmers.se>
2024-09-18 15:03:05 +0200

    fix: path to human-GEM repo

--- code/test/macawTests.py
+++ code/test/macawTests.py
@@ -1,7 +1,7 @@
 import cobra
 from macaw.main import dead_end_test, duplicate_test
 
-model = cobra.io.load_yaml_model("Human-GEM/model/Human-GEM.yml")
+model = cobra.io.load_yaml_model("model/Human-GEM.yml")
 (dead_end_results, dead_end_edges) = dead_end_test(model)
 (duplicate_results, duplicate_edges) = duplicate_test(model)
 output = dead_end_results.merge(duplicate_results)
