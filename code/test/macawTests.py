838f3b49035a5aff0e5b4c3c78885f199e0900d2
Eduard Kerkhoven <eduardk@chalmers.se>
2024-09-18 14:57:29 +0200

    feat: first macaw test scripts

---
+++ code/test/macawTests.py
@@ -0,0 +1,8 @@
+import cobra
+from macaw.main import dead_end_test, duplicate_test
+
+model = cobra.io.load_yaml_model("Human-GEM/model/Human-GEM.yml")
+(dead_end_results, dead_end_edges) = dead_end_test(model)
+(duplicate_results, duplicate_edges) = duplicate_test(model)
+output = dead_end_results.merge(duplicate_results)
+output.to_csv('Human-GEM/data/macaw_dead_end_and_duplicate_test_results.csv', index = False)
